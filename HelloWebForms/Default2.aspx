3c8a85246921ec6aa432fbea60ce23ca268e99ae
Glynnis Thomas <glynnisthomas@gmail.com>
2019-01-29 21:50:00 -0600

    Added another basic page

---
+++ HelloWebForms/Default2.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox><asp:Button ID="Button1" runat="server" Text="It's a Button" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ HelloWebForms/Default2.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextBox1.Text = "Loading";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        TextBox1.Text = "You clicked it!";
+        Response.Write("Great job!");
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
